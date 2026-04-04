f4be9fcd985127b54822b341dafa985f5466e164
Gianluca Poli <gianluca.poli8@studio.unibo.it>
2026-04-04 16:08:04 +0200

    getter and setter method for pawn added

--- src/main/java/gioco_scudetto/model/api/Club.java
+++ src/main/java/gioco_scudetto/model/api/Club.java
@@ -5,11 +5,13 @@ public interface Club {
     String getName();
     int getPoints();
     int getNetDiff();
+    Pawn getPawn();
 
     //Setter Methods
     void setName(String name);
     void incrementPoints(int points);
     void changeNetDiffs(int goalScored, int goalConceded);
+    void changePawn(Pawn pawnSelected);
 
     //AGGIUNGERE I METODI PER SETTARE E GETTARE LE PEDINE
 }
