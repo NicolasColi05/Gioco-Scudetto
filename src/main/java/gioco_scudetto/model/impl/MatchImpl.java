9e236d681538e34940e71aa62490e7448576d13e
Federico Cecchini <federico.cecchini7@studio.unibo.it>
2026-04-07 18:32:51 +0200

    created MatchImpl

---
+++ src/main/java/gioco_scudetto/model/impl/MatchImpl.java
@@ -0,0 +1,54 @@
+package gioco_scudetto.model.impl;
+
+import gioco_scudetto.model.api.Club;
+import gioco_scudetto.model.api.Match;
+import gioco_scudetto.model.api.Scoreboard;
+
+public class MatchImpl implements Match{
+
+    private Club clubHome;
+    private Club clubAway;
+    private int goalHome;
+    private int goalAway;
+
+    @Override
+    public Club turn() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'turn'");
+    }
+
+    @Override
+    public void goalHome() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'goalHome'");
+    }
+
+    @Override
+    public void goalAway() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'goalAway'");
+    }
+
+    @Override
+    public Club getClubHome() {
+        return clubHome;
+    }
+
+    @Override
+    public Club getClubAway() {
+        return clubAway;
+    }
+
+    @Override
+    public Scoreboard getScore() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getScore'");
+    }
+
+    @Override
+    public int getMatchStatus() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMatchStatus'");
+    }
+    
+}
