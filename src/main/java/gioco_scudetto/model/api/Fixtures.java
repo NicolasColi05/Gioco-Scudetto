1d9cb4f8292e0a6c43ca58cb84ab3b4d93ae100d
Federico Cecchini <federico.cecchini7@studio.unibo.it>
2026-04-07 18:18:00 +0200

    added some methods in interface match and fixtures

--- src/main/java/gioco_scudetto/model/api/Fixtures.java
+++ src/main/java/gioco_scudetto/model/api/Fixtures.java
@@ -2,6 +2,8 @@ package gioco_scudetto.model.api;
 
 public interface Fixtures {
     
+    void generateFixture();
+    
     Pair<Club,Club> getNextMatch();
     
     Pair<Club,Club> getCurrentMatch();
--- src/main/java/gioco_scudetto/model/api/Match.java
+++ src/main/java/gioco_scudetto/model/api/Match.java
@@ -2,11 +2,17 @@ package gioco_scudetto.model.api;
 
 public interface Match {
 
+    Club turn();
+    
+    void goalHome();
+
+    void goalAway();
+
     Club getClubHome();
 
     Club getClubAway();
 
     Scoreboard getScore();
 
-    int getMatchStatus();
+    int getMatchStatus(); //enum StatoPartita { IN_CORSO, FINE_PRIMO_TEMPO, FINE_PARTITA }
 }
