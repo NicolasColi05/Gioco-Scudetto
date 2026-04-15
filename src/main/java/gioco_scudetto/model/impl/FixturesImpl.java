cac9a571cdba643a2bb37228afd4c3dd875916c8
Federico Cecchini <federico.cecchini7@studio.unibo.it>
2026-04-15 10:58:41 +0200

    added methods in fixturesImpl

--- src/main/java/gioco_scudetto/model/impl/FixturesImpl.java
+++ src/main/java/gioco_scudetto/model/impl/FixturesImpl.java
@@ -4,4 +4,21 @@ import gioco_scudetto.model.api.Fixtures;
 
 public class FixturesImpl implements Fixtures{
     
+    int NumCLubs;
+    
+    @Override
+    public void generateFixtures() {
+
+    }
+
+    @Override
+    public Pair<Club,Club> getNextMatch() {
+
+    }
+
+    @Override
+    public Pair<Club,Club> getCurrentMatch() {
+
+    }
+
 }
