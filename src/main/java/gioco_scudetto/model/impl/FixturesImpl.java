7c2652cb237208d6286181042458c66878dd4ac0
Gianluca Poli <gianluca.poli8@studio.unibo.it>
2026-04-15 11:12:03 +0200

    fixed error in FixturesImpl and added Jcomponent to frame

--- src/main/java/gioco_scudetto/model/impl/FixturesImpl.java
+++ src/main/java/gioco_scudetto/model/impl/FixturesImpl.java
@@ -1,7 +1,27 @@
 package gioco_scudetto.model.impl;
 
+import gioco_scudetto.model.api.Club;
 import gioco_scudetto.model.api.Fixtures;
+import gioco_scudetto.model.api.Pair;
 
 public class FixturesImpl implements Fixtures{
+
+    @Override
+    public void generateFixture() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'generateFixture'");
+    }
+
+    @Override
+    public Pair<Club, Club> getNextMatch() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getNextMatch'");
+    }
+
+    @Override
+    public Pair<Club, Club> getCurrentMatch() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCurrentMatch'");
+    }
     
 }
