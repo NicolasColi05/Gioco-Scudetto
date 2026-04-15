16561cdd88ae5949f64a0866028c6a7b42451a08
Nicolas Colinucci <nicolas.colinucci@studio.unibo.it>
2026-04-15 09:58:21 +0200

    modified goalnet

--- src/main/java/gioco_scudetto/model/api/GoalNet.java
+++ src/main/java/gioco_scudetto/model/api/GoalNet.java
@@ -3,7 +3,8 @@ package gioco_scudetto.model.api;
 
 public interface GoalNet {
 
-    void setGoalKeeperPosition(int position); //in base alla prima posiizione alcune non saranno selezionabili ma lo devo controllare nel controller
+    void setGoalKeeperPosition(int position); //in base alla prima posiizione alcune non saranno selezionabili ma lo devo controllare nel controller,
+                                             // es: se il portiere è in posizione 1 posso scegliere solo tra 2,4,5
 
     boolean isGoal();
 }
