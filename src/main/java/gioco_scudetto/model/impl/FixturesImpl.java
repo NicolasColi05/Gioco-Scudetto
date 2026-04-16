7383196b827842810cb9f1310d731d19d55b0c33
Federico Cecchini <federico.cecchini7@studio.unibo.it>
2026-04-16 19:11:17 +0200

    changed the constructor in fixtureImpl and added a private method

--- src/main/java/gioco_scudetto/model/impl/FixturesImpl.java
+++ src/main/java/gioco_scudetto/model/impl/FixturesImpl.java
@@ -1,5 +1,8 @@
 package gioco_scudetto.model.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import gioco_scudetto.model.api.Club;
 import gioco_scudetto.model.api.Fixtures;
 import gioco_scudetto.model.api.Pair;
@@ -7,11 +10,18 @@ import gioco_scudetto.model.api.Pair;
 public class FixturesImpl implements Fixtures{
 
     private int numCLubs;
+    private List<Club> listOfClubs;
+    private List<Pair<Club,Club>> fixture;
     
-    public FixturesImpl(int numClubs){
-        this.numCLubs = numClubs;
+    public FixturesImpl(List<Club> listOfClubs) {
+        this.listOfClubs = listOfClubs;
+        this.numCLubs = listOfClubs.size();
+        this.fixtureGeneration();
     }
+    /*utilizzo pattern builder, ciò porta a dover chiamare il costruttore due volte prima con il 
+    numero di club e poi con i nomi dei club che può essere sotto forma di lista o meno
 
+    anzi credo che basti passare una lista con i nomi dei club almeno ho sia i nomi che la lunghezza*/
     @Override
     public Pair<Club,Club> getNextMatch() {
         return null;
@@ -20,6 +30,12 @@ public class FixturesImpl implements Fixtures{
     @Override
     public Pair<Club,Club> getCurrentMatch() {
         return null;
+    }
+
+    private void fixtureGeneration(){
+        this.fixture = new ArrayList<>();
+        
+        return;
     }
 
 }
