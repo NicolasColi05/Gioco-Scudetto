74ce589504cc89d51c3906577481b0cc4bbf06b1
Gianluca Poli <gianluca.poli8@studio.unibo.it>
2026-04-19 23:06:05 +0200

    Fixed all the button position, fixed font size responsively to the resolution changes and changed the private method createComponent to use it not only for buttons

--- src/main/java/gioco_scudetto/view/impl/HomeView.java
+++ src/main/java/gioco_scudetto/view/impl/HomeView.java
@@ -14,51 +14,74 @@ public class HomeView {
 
         //Setting screen responsive resolution and placing it in the center
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        int width = (int) screenSize.getWidth();
-        int height = (int) screenSize.getHeight();
+        int minimumWidht = screenSize.width / 2;
+        int minimumHeight = screenSize.height / 2;
 
-        frame.setSize(width / 2, height / 2);
+        frame.setSize(minimumWidht , minimumHeight);
         frame.setResizable(true);
-        frame.setLocationRelativeTo(null);
+        frame.setMinimumSize(new Dimension(minimumWidht, minimumHeight));
 
         //Creating different font for each component
-        Font titleFont = new Font("Monospaced", Font.BOLD, 48);
-        Font buttonFont = new Font("Monospaced", Font.BOLD, 36);
-        Font exitFont = new Font("Monospaced", Font.PLAIN, 18);
+        Font titleFont = new Font("Monospaced", Font.BOLD, minimumWidht / 30);
+        Font buttonFont = new Font("Monospaced", Font.BOLD, minimumWidht / 50);
+        Font exitFont = new Font("Monospaced", Font.BOLD, minimumWidht / 80);
 
         //Creating the mainframe panel
         final JPanel mainPanel = new JPanel(new BorderLayout());
 
         //Adding Game Title and setting it in the top center position of the frame
-        JLabel gameTitle = new JLabel("GIOCO DELLO SCUDETTO",SwingConstants.CENTER);
+        JComponent gameTitle = createComponent(new JLabel("GIOCO DELLO SCUDETTO", SwingConstants.CENTER), titleFont, Color.RED);
+        //JLabel gameTitle = new JLabel("GIOCO DELLO SCUDETTO",SwingConstants.CENTER);
         gameTitle.setForeground(Color.RED);
         gameTitle.setFont(titleFont);
         mainPanel.add(gameTitle, BorderLayout.NORTH);
 
-        //Creating buttons to select to play with bots or friend, and to close the game
-        JButton selectBot = createSelectionButton("<html><center>PLAY WITH<br>BOTS</center></html>", buttonFont);
-        JButton selectFriend = createSelectionButton("<html><center>PLAY WITH<br>FRIENDS</center></html>", buttonFont);
-        JButton exitGame = new JButton("EXIT");
+        //Creating buttons to select to play with bots or friend
+        JPanel selectButtonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 80, 0));
+        JComponent selectBot = createComponent(new JButton("<html><center>PLAY WITH<br>BOTS</center></html>"), buttonFont, Color.BLUE);
+        JComponent selectFriend = createComponent(new JButton( "<html><center>PLAY WITH<br>FRIENDS</center></html>"), buttonFont, Color.BLUE);
+        selectButtonPanel.add(selectBot); 
+        selectButtonPanel.add(selectFriend);       
 
-        mainPanel.add(selectBot, BorderLayout.EAST);
-        mainPanel.add(selectFriend, BorderLayout.WEST);
-        mainPanel.add(exitGame, BorderLayout.SOUTH);
+        //Centralizing button vertically and responsively to the resolution changes
+        JPanel centerWrapper = new JPanel(new GridBagLayout());
+        centerWrapper.add(selectButtonPanel);
+
+        //Creating button to exit from the game
+        JPanel exitButtonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JComponent exitGame = createComponent(new JButton("EXIT"), exitFont, Color.BLACK);
+        exitButtonPanel.add(exitGame);
+
+        mainPanel.add(centerWrapper, BorderLayout.CENTER);
+        mainPanel.add(exitButtonPanel, BorderLayout.SOUTH);
         
-        
+        //Listener to change responsivly font size dipending by resolution changes
+        frame.addComponentListener(new java.awt.event.ComponentAdapter() {
+            @Override
+            public void componentResized(java.awt.event.ComponentEvent e) {
+                
+                int currentWidth = frame.getWidth();
+
+                gameTitle.setFont(new Font("Monospaced", Font.BOLD, currentWidth / 15));
+                selectBot.setFont(new Font("Monospaced", Font.BOLD, currentWidth / 25));
+                selectFriend.setFont(new Font("Monospaced", Font.BOLD, currentWidth / 25));
+                exitGame.setFont(new Font("Monospaced", Font.BOLD, currentWidth / 40));
+                
+                frame.revalidate();
+            }
+        });
 
         //Setting frame main panel
         frame.getContentPane().add(mainPanel);
-
-        frame.getContentPane().add(mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 
-    private JButton createSelectionButton(final String name, Font font) {
-        JButton button = new JButton(name);
-        button.setFont(font);
-        button.setForeground(Color.BLUE);
-        return button;
+    private JComponent createComponent(final JComponent component, final Font font, final Color color) {
+        component.setFont(font);
+        component.setForeground(color);
+        return component;
     }
 
 }
