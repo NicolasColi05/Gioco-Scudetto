3949f65779b62e3b655a23dcde6c3f76ea031c88
Gianluca Poli <gianluca.poli8@studio.unibo.it>
2026-04-19 10:30:54 +0200

    Setted default dimensione when opening the HomeView

--- src/main/java/gioco_scudetto/view/impl/HomeView.java
+++ src/main/java/gioco_scudetto/view/impl/HomeView.java
@@ -1,18 +1,28 @@
 package gioco_scudetto.view.impl;
 
 import javax.swing.*;
+import java.awt.*;
 import gioco_scudetto.controller.api.Starter;
 
 public class HomeView {
     
     private final JFrame frame = new JFrame("GIOCO DELLO SCUDETTO");
-    //private final Starter starter;
+    private final Starter starter;
 
     public HomeView(final Starter starter) {
-        //this.starter = starter;
+        this.starter = starter;
+
+        //Setting screen responsive resolution and placing it in the center
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = (int) screenSize.getWidth();
+        int height = (int) screenSize.getHeight();
+
+        frame.setSize(width / 2, height / 2);
+        frame.setLocationRelativeTo(null);
+
         frame.add(new JPanel());
 
-
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
 
