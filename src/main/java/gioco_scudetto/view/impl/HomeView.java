3eb94353cef90aab712e6163f96bc9f560bb4990
Gianluca Poli <gianluca.poli8@studio.unibo.it>
2026-04-19 10:53:03 +0200

    Created some inital font and the JLabel for the game Title

--- src/main/java/gioco_scudetto/view/impl/HomeView.java
+++ src/main/java/gioco_scudetto/view/impl/HomeView.java
@@ -18,10 +18,28 @@ public class HomeView {
         int height = (int) screenSize.getHeight();
 
         frame.setSize(width / 2, height / 2);
+        frame.setResizable(true);
         frame.setLocationRelativeTo(null);
 
-        frame.add(new JPanel());
+        //Creating different font for each component
+        Font titleFont = new Font("Monospaced", Font.BOLD, 48);
+        Font buttonFont = new Font("Monospaced", Font.BOLD, 36);
+        Font exitFont = new Font("Monospaced", Font.PLAIN, 18);
 
+        //Creating the mainframe panel
+        final JPanel mainPanel = new JPanel(new BorderLayout());
+
+        //Adding Game Title and setting it in the top center position of the frame
+        JLabel gameTitle = new JLabel("GIOCO DELLO SCUDETTO",SwingConstants.CENTER);
+        gameTitle.setForeground(Color.RED);
+        gameTitle.setFont(titleFont);
+        mainPanel.add(gameTitle, BorderLayout.NORTH);
+
+
+        //Setting frame main panel
+        frame.getContentPane().add(mainPanel);
+
+        frame.getContentPane().add(gameTitle);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
