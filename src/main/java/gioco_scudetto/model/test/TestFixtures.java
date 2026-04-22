aae6c3089bd0bb6b7ad96f874b59092b03e75311
Federico Cecchini <federico.cecchini7@studio.unibo.it>
2026-04-22 18:21:02 +0200

    created file for test of the fixture

---
+++ src/main/java/gioco_scudetto/model/test/TestFixtures.java
@@ -0,0 +1,15 @@
+package gioco_scudetto.model.test;
+
+import gioco_scudetto.model.impl.FixturesImpl;
+import gioco_scudetto.model.impl.ClubImpl;
+import gioco_scudetto.model.api.Pawn;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestFixturesImpl {
+    //FixturesImpl fixtures = new FixturesImpl();
+}
