a237c35c20bfca4b3c33c617e77892cf5894acaa
Federico Cecchini <federico.cecchini7@studio.unibo.it>
2026-04-23 15:21:21 +0200

    started writing the file testFixture but i need the pawn implementation and fixed an error about the classpath

--- src/test/java/gioco_scudetto/model/TestFixtures.java
+++ src/test/java/gioco_scudetto/model/TestFixtures.java
@@ -1,8 +1,24 @@
 package gioco_scudetto.model;
 
+
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions.*;
+
+import gioco_scudetto.model.api.Fixtures;
+import gioco_scudetto.model.api.Pawn;
+import gioco_scudetto.model.api.Club;
+import gioco_scudetto.model.impl.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
 
 public class TestFixtures {
+    Club roma = new ClubImpl(new Pawn());
+    Club inter = new ClubImpl(new Pawn());
+    Club napoli = new ClubImpl(new Pawn());
+    Club juventus = new ClubImpl(new Pawn());
+    ArrayList<Club> listOfClubs = new ArrayList<>();
     
+
+    //Fixtures fixtures = new FixturesImpl();
 }
