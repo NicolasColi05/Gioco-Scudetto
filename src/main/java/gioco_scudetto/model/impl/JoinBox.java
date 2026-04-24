b6c2814cb782299d52f6ad3a14d36b3e59931be3
Pasqui088 <diego.pasquinelli2@studio.unibo.it>
2026-04-24 10:17:57 +0200

    implementazione casella Unione

---
+++ src/main/java/gioco_scudetto/model/impl/JoinBox.java
@@ -0,0 +1,45 @@
+package gioco_scudetto.model.impl;
+
+import gioco_scudetto.model.api.Boxes;
+import gioco_scudetto.model.api.Match;
+import gioco_scudetto.model.api.Club;
+
+
+
+public class JoinBox implements Boxes {
+    private final int position;
+    public JoinBox(int position) {
+        this.position = position;
+    }
+
+    @Override
+    public int getPosition() {
+        return position;
+    }
+
+    @Override
+    public String getName() {
+        return "Join Box";
+    }
+
+    @Override
+    public void event (Match match) {
+        Club current;
+        Club opponent;
+
+        if (match.getMatchStatus() == 0) {
+            current = match.getClubHome();
+            opponent = match.getClubAway();
+        } else {
+            current = match.getClubAway();
+            opponent = match.getClubHome();
+        }
+
+        System.out.println(current + "entered Join Box");
+
+        int currentPosition = current.getPawn().getPosition();
+        opponent.getPawn().setPosition(currentPosition);
+
+        System.out.println(opponent + "joined current player at position" + currentPosition);
+    }
+}
