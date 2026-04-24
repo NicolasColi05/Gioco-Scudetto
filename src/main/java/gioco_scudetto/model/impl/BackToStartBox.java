6fc0dcfc4012e248f6d6d84d8cd21393ab81b9da
Pasqui088 <diego.pasquinelli2@studio.unibo.it>
2026-04-24 09:41:17 +0200

    implementazione casella Torna all'inizio

---
+++ src/main/java/gioco_scudetto/model/impl/BackToStartBox.java
@@ -0,0 +1,36 @@
+package gioco_scudetto.model.impl;
+
+import gioco_scudetto.model.api.Boxes;
+import gioco_scudetto.model.api.Match;
+import gioco_scudetto.model.api.Club;
+
+public class BackToStartBox implements Boxes {
+    private final int position;
+    public BackToStartBox( int position) {
+        this.position = position;
+    }
+
+    @Override
+    public int getPosition() {
+        return position;
+    }
+
+    @Override
+    public String getName() {
+        return "Back to Start";
+    }
+
+    @Override
+    public void event(Match match) {
+        Club club;
+
+        if (match.getMatchStatus() == 0) {
+            club = match.getClubHome();
+        } else {
+            club = match.getClubAway();
+        }
+
+        club.getPawn().setPosition(0);
+        System.out.println(club + "go back to start");
+    }
+}
