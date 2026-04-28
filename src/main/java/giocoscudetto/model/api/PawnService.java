7271e6d85635dacdca8cc42d16718345f6890338
Federico Cecchini <federico.cecchini7@studio.unibo.it>
2026-04-28 13:35:52 +0200

    deleted the pawn interface because not used

--- src/main/java/giocoscudetto/model/api/PawnService.java
+++
@@ -1,7 +0,0 @@
-package giocoscudetto.model.api;
-
-public interface PawnService {
-    void changePosition(Club club, int steps);
-    int getPosition(Club club);
-    void setPosition(Club club, int position);
-}
