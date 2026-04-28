cef3818aeb2005c56ddb87e1c7d826d96f232ba8
Federico Cecchini <federico.cecchini7@studio.unibo.it>
2026-04-28 14:02:29 +0200

    fixed some errors

--- src/main/java/giocoscudetto/model/api/Pawn.java
+++ src/main/java/giocoscudetto/model/api/Pawn.java
@@ -1,6 +1,6 @@
 package giocoscudetto.model.api;
 
-public interface PawnService {
+public interface Pawn {
     void changePosition(Club club, int steps);
     int getPosition(Club club);
     void setPosition(Club club, int position);
