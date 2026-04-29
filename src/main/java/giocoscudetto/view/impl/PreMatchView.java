66a909d148d5322eeac465e551aa235f3b1c221f
Federico Cecchini <federico.cecchini7@studio.unibo.it>
2026-04-29 13:36:15 +0200

    working on PreMatchView

--- src/main/java/giocoscudetto/view/impl/PreMatchView.java
+++ src/main/java/giocoscudetto/view/impl/PreMatchView.java
@@ -29,6 +29,9 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 
 import giocoscudetto.view.impl.ClubPanel;
+import giocoscudetto.model.api.Pair;
+import giocoscudetto.model.impl.ClubImpl;
+import giocoscudetto.model.impl.PawnImpl;
 import giocoscudetto.model.impl.TableImpl;
 import giocoscudetto.controller.api.Starter;
 
@@ -40,6 +43,11 @@ public class PreMatchView extends DefaultPanelImpl{
         {"Inter", 6, 3},
         {"Roma", 7, 2}
     };
+    private static String[] columnNames2 = {"Clubs", "Results"};
+    private Object[][] dati2 = {
+        {new Pair<String, String>(new ClubImpl(new PawnImpl()).getName(), new ClubImpl(new PawnImpl()).getName()), "0-0"},
+        {new Pair("Inter", "Roma"), "0-0"}
+    };
 
     private final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     private final int minimumWidht = screenSize.width / 2;
@@ -50,16 +58,24 @@ public class PreMatchView extends DefaultPanelImpl{
 
         this.setLayout(new BorderLayout());
 
+
         final JComponent gameTitle = createComponent(new JLabel("GIOCO DELLO SCUDETTO", SwingConstants.CENTER), getTitleFont(), Color.RED);
         this.add(gameTitle, BorderLayout.NORTH);
 
-        final JTable table = (JTable) createComponent(new JTable(dati, columnNames), getTitleFont(), Color.RED);
-        table.setBackground(Color.WHITE);
-        table.setEnabled(false);
-        table.setFont(new Font(FONT_SELECTED, Font.BOLD, minimumWidht / BUTTON_FONT_REDUCTION));
+        final JTable fixtureTable = (JTable) createComponent(new JTable(dati2, columnNames2), getTitleFont(), Color.RED);
+        fixtureTable.setBackground(Color.WHITE);
+        fixtureTable.setEnabled(false);
+        fixtureTable.setFont(new Font(FONT_SELECTED, Font.BOLD, minimumWidht / BUTTON_FONT_REDUCTION));
+
+        final JTable table2 = (JTable) createComponent(new JTable(dati, columnNames), getTitleFont(), Color.RED);
+        table2.setBackground(Color.WHITE);
+        table2.setEnabled(false);
+        
+        table2.setFont(new Font(FONT_SELECTED, Font.BOLD, minimumWidht / BUTTON_FONT_REDUCTION));
 
         this.add(gameTitle, BorderLayout.NORTH);
-        this.add(new JScrollPane(table), BorderLayout.WEST);
+        this.add(new JScrollPane(fixtureTable), BorderLayout.WEST);
+        this.add(new JScrollPane(table2), BorderLayout.EAST);
     }
 
 }
