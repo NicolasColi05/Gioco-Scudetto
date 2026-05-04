c9614f91b8ce97b41d8c77935b74f769175daba4
Gianluca Poli <gianluca.poli8@studio.unibo.it>
2026-05-04 11:34:02 +0200

    Fixed javadoc comment in Club, Table interfaces and their classes implementation

--- src/main/java/giocoscudetto/model/impl/ClubImpl.java
+++ src/main/java/giocoscudetto/model/impl/ClubImpl.java
@@ -3,59 +3,89 @@ package giocoscudetto.model.impl;
 import giocoscudetto.model.api.Club;
 import giocoscudetto.model.api.Pawn;
 
+/**
+ * Implementing Club interface
+ */
 public class ClubImpl implements Club{
     
-    //Class Fields
+    //Class Fields not final cause they can ben changed.
     private String name;
     private Pawn pawn;
     private int points;
     private int netDiff;
 
-    //Class Constructor
-    public ClubImpl(final Pawn pawnSelected) {
-         //Setting a standard name and pawn selected
-        this.pawn = pawnSelected;
-        this.name = "STANDARD";
+    /**
+     * Constructor to set the name and pawn of the t.
+     * 
+     * @param name
+     * @param pawnSelected
+     */
+    public ClubImpl(final String name, final Pawn pawn) {
+        this.name = name;
+        this.pawn = pawn;
     }
 
-    //Class Method
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getName() {
         return this.name;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getPoints() {
         return this.points;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getNetDiff() {
         return this.netDiff;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Pawn getPawn() {
         return this.pawn;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setName(final String name) {
         this.name = name;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void incrementPoints(final int points) {
         this.points += points;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void changeNetDiffs(final int goalScored, final int goalConceded) {
-        this.netDiff += (goalScored + goalConceded);
+        this.netDiff += (goalScored - goalConceded);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void changePawn(final Pawn pawnSelected) {
+    public void setPawn(final Pawn pawnSelected) {
         this.pawn = pawnSelected;
     }
 }
