a98d22df584f26de92b68d7766e68a333604dcb9
Nicolas Colinucci <nicolas.colinucci@studio.unibo.it>
2026-05-07 15:28:11 +0200

    added a method for settig the rigth text using the Thread

--- src/main/java/giocoscudetto/view/impl/DicePanel.java
+++ src/main/java/giocoscudetto/view/impl/DicePanel.java
@@ -3,10 +3,12 @@ package giocoscudetto.view.impl;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.lang.reflect.InvocationTargetException;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 import javax.swing.border.EmptyBorder;
 
 import giocoscudetto.controller.api.Starter;
@@ -14,11 +16,13 @@ import giocoscudetto.controller.api.Starter;
 public class DicePanel extends DefaultPanelImpl {
     
     private final Starter controller;
+    private final JLabel messageLabel;
+
     public DicePanel(final Starter controller) {
         this.controller = controller;
         this.setLayout(new BorderLayout());
         this.setBackground(Color.WHITE);
-        JLabel messageLabel = new JLabel( ", it's your turn!");
+        messageLabel = new JLabel();
         messageLabel.setFont(new Font("SansSerif", Font.PLAIN, 12));
         messageLabel.setHorizontalAlignment(SwingConstants.CENTER);
         messageLabel.setBorder(new EmptyBorder(8, 4, 8, 4));
@@ -31,5 +35,25 @@ public class DicePanel extends DefaultPanelImpl {
         rollDiceButton.addActionListener(e -> {
                 this.controller.move();
             });
+    }
+
+    private final class Agent implements Runnable {
+        /**
+         * 
+         */
+        private volatile boolean stop;
+
+        @Override
+        public void run() {
+            while (!this.stop) {
+                try {
+                    SwingUtilities.invokeAndWait(() -> DicePanel.this.messageLabel.setText(DicePanel.this.controller.getDescription()));
+                    Thread.sleep(100);
+                } catch (InvocationTargetException | InterruptedException ex) {
+                        ex.printStackTrace(); //NOPMD
+                }
+               
+            }
+        }
     }
 }
