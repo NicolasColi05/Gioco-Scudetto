6b94978fa2b22b64cba15632c06ad860c7d9ca84
Nicolas Colinucci <nicolas.colinucci@studio.unibo.it>
2026-05-11 16:32:11 +0200

    changing the impl of match panel

--- src/test/java/giocoscudetto/view/TestMatchPanel.java
+++ src/test/java/giocoscudetto/view/TestMatchPanel.java
@@ -1,9 +1,13 @@
 package giocoscudetto.view;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
+import giocoscudetto.controller.api.Starter;
 import giocoscudetto.controller.impl.StarterImpl;
 
 public class TestMatchPanel extends JFrame{
@@ -11,7 +15,9 @@ public class TestMatchPanel extends JFrame{
     public TestMatchPanel() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(650, 670);
-        JPanel pa = new giocoscudetto.view.impl.MatchPanel(new StarterImpl(null));
+        Starter s = new StarterImpl(null);
+        s.setClubs(new ArrayList<>(List.of("Fede","Nico")), new ArrayList<>(List.of("giallo","rosso")));
+        JPanel pa = new giocoscudetto.view.impl.MatchPanel(s);
         this.setContentPane(pa);
         this.setVisible(true);
     }
@@ -19,6 +25,7 @@ public class TestMatchPanel extends JFrame{
 
 
     public static void main(String[] args) {
+
         SwingUtilities.invokeLater(() -> new TestMatchPanel());
 
     }
