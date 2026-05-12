87b3d1438fc19b075996e2cdab0db9d38d146c0d
Pasqui088 <diego.pasquinelli2@studio.unibo.it>
2026-05-12 16:45:40 +0200

    fix TestEndGameView

--- src/test/java/giocoscudetto/model/TestEndGameView.java
+++ src/test/java/giocoscudetto/model/TestEndGameView.java
@@ -11,14 +11,14 @@ public class TestEndGameView extends JFrame {
 public TestEndGameView() {
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     this.setSize(650, 670);
-    JPanel pa = new giocoscudetto.view.impl.EndGameView(new StarterImpl(null));
+    JPanel pa = new giocoscudetto.view.impl.EndGameView(new StarterImpl(null, null));
     this.setContentPane(pa);
-    this.setVisible(true);
-}
-
+     this.setVisible(true);
+    }
 
 public static void main(String[] args) {
     SwingUtilities.invokeLater(() -> new TestEndGameView());
 
-    }
+ }
 }
+
