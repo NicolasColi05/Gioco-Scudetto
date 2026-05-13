5841439e37e8c06ce6760349e521c6f142ab4a9a
Federico Cecchini <federico.cecchini7@studio.unibo.it>
2026-05-13 15:21:34 +0200

    created the last box

---
+++ src/main/java/giocoscudetto/model/impl/FinishBox.java
@@ -0,0 +1,41 @@
+package giocoscudetto.model.impl;
+
+import giocoscudetto.model.api.Boxes;
+import giocoscudetto.model.api.Match;
+
+public class FinishBox implements Boxes{
+
+    private final int position;
+    private final String image;
+    private final String description = "Box event: Last Box. If you land on this box the game will end";
+
+    public FinishBox(final int position){
+        this.position = position;
+        this.image = "caselle_precise/casella_32.png";
+    }
+    @Override
+    public int getPosition() {
+        return this.position;
+    }
+
+    @Override
+    public void event(Match match) {
+        
+    }
+
+    @Override
+    public String getName() {
+        return "Finish Box";
+    }
+
+    @Override
+    public String getImage() {
+        return this.image;
+    }
+
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+    
+}
