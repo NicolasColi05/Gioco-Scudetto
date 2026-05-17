0403c8c68df4947b6c1e86a8d03394fd0c12de1d
Federico Cecchini <federico.cecchini7@studio.unibo.it>
2026-05-17 18:06:19 +0200

    added the fact that now the fixtureTable in the PreMatchView is now responsive to visually and doesn't keep record of the previous championships mixing the new and the old championship

--- src/main/java/giocoscudetto/view/impl/creation/ClubPanel.java
+++ src/main/java/giocoscudetto/view/impl/creation/ClubPanel.java
@@ -129,6 +129,9 @@ public class ClubPanel extends DefaultPanelImpl{
         }); 
         
         btnCont.addActionListener(e -> { 
+            if(!controller.getFixture().isEmpty()){
+                viewChanger.resetFixture();
+            }
             //Creating clubs, table and fixtures to start the match
             this.controller.createClubs(this.clubsName.stream()
                                             .map(JTextField::getText)
