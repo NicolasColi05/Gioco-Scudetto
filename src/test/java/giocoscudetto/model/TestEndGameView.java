e8acf1995f8a9d29d4003bfe23197d95c93c8251
Pasqui088 <diego.pasquinelli2@studio.unibo.it>
2026-05-20 08:40:48 +0200

    add ranking table and fix TestEndGameView

--- src/test/java/giocoscudetto/model/TestEndGameView.java
+++ src/test/java/giocoscudetto/model/TestEndGameView.java
@@ -1,17 +1,29 @@
 package giocoscudetto.model;
-
+import java.util.List;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
+import giocoscudetto.controller.impl.CreateUpdateControllerImpl;
 import giocoscudetto.controller.impl.StarterImpl;
+import giocoscudetto.model.impl.ClubImpl;
+import giocoscudetto.model.impl.MatchImpl;
+import giocoscudetto.model.impl.PawnImpl;
 
 public class TestEndGameView extends JFrame {
 
 public TestEndGameView() {
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     this.setSize(650, 670);
-    JPanel pa = new giocoscudetto.view.impl.EndGameView(new StarterImpl(null, null), "Roma");
+    var cr=new CreateUpdateControllerImpl();
+    cr.createClubs(List.of("Milan","Juve","Roma","Inter"), List.of(255,128,10,255,128,10));
+    var starter=new StarterImpl(null, cr);
+    cr.getTable().showPosition().get(0).changeNetDiffs(12, 10);
+    
+    starter.setMatch();
+    
+    starter.addPoints();
+    JPanel pa = new giocoscudetto.view.impl.EndGameView(starter, "Roma");
     this.setContentPane(pa);
      this.setVisible(true);
     }
