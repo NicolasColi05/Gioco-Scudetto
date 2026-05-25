dd6ca62f0a6b854a031c04b30e7e730bb87db0da
Nicolas Colinucci <nicolas.colinucci@studio.unibo.it>
2026-05-25 18:52:51 +0200

    implemented the test for resultBox

--- src/test/java/giocoscudetto/model/boxestest/TestResultBox.java
+++ src/test/java/giocoscudetto/model/boxestest/TestResultBox.java
@@ -3,32 +3,60 @@ package giocoscudetto.model.boxestest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import giocoscudetto.model.api.*;
 import giocoscudetto.model.impl.ClubImpl;
 import giocoscudetto.model.impl.MatchImpl;
-import giocoscudetto.model.impl.boxes.CesariniBox;
+import giocoscudetto.model.impl.boxes.ResultBox;
 
 public class TestResultBox {
-    private final Club clubHome = new ClubImpl("home", null);
-    private final Club clubAway = new ClubImpl("away", null);
-    private final Match match = new MatchImpl(clubHome, clubAway);
-    private final Scoreboard scoreboard = match.getScore();
-    private final Boxes cesariniBox = new CesariniBox(0);
+    private Club clubHome;
+    private Club clubAway;
+    private Match match;
+    private ResultBox resultBox;
 
-    
     @BeforeEach
-    public void setUpCurrentPlayer() {
-
-        //Setting initially the current club the home club, and fixing match status
-        //to have the homeClub as the current Club
-        if(match.getCurrentPlayer() != clubHome) {
-            match.turn();
-        }
+    public void setUp() {
+        clubHome = new ClubImpl("home", null);
+        clubAway = new ClubImpl("away", null);
+        match = new MatchImpl(clubHome, clubAway);
+        resultBox = new ResultBox(5);
     }
 
     @Test
-    public void testBox() {
+    public void testEventSetsGameModeToResult() {
+        assertNotEquals("RESULT", match.getGameMode());
+        resultBox.event(match);
+        assertEquals("RESULT", match.getGameMode());
+    }
 
-        
+    @Test
+    public void testResultBoxPosition() {
+        assertEquals(5, resultBox.getPosition());
+    }
+
+    @Test
+    public void testResultBoxName() {
+        assertEquals("result box", resultBox.getName());
+    }
+
+    @Test
+    public void testResultBoxImage() {
+        assertEquals("casella_3.png", resultBox.getImage());
+    }
+
+    @Test
+    public void testResultEvent() {
+        final int dice1;
+        final int dice2;
+
+        resultBox.event(match);
+        dice1 = match.diceEvent();
+        dice2 = match.diceEvent();
+
+        assertEquals(dice1, match.getScore().getHomeScore());
+        assertEquals(dice2, match.getScore().getGuestScore());
+        assertEquals(clubAway, match.getCurrentPlayer());
     }
 }
