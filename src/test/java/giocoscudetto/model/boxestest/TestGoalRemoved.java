5f30b78e9f25337eff29b7e48484028c48a4c2e1
Gianluca Poli <gianluca.poli8@studio.unibo.it>
2026-05-26 13:05:07 +0200

    TestGoalRemoved completed

--- src/test/java/giocoscudetto/model/boxestest/TestGoalRemoved.java
+++ src/test/java/giocoscudetto/model/boxestest/TestGoalRemoved.java
@@ -1,5 +1,96 @@
 package giocoscudetto.model.boxestest;
 
+import org.junit.jupiter.api.Test;
+
+import giocoscudetto.model.api.Boxes;
+import giocoscudetto.model.api.Club;
+import giocoscudetto.model.api.Match;
+import giocoscudetto.model.api.Scoreboard;
+import giocoscudetto.model.impl.ClubImpl;
+import giocoscudetto.model.impl.MatchImpl;
+import giocoscudetto.model.impl.boxes.GoalRemovedBox;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+
+/*
+ * CHECKSTYLE: MagicNumber OFF
+ * The above comment shuts down checkstyle: in a test suite, magic numbers may be tolerated.
+ */
+/**
+ * Test for {@link giocoscudetto.model.impl.boxes.GoalRemovedBox}.
+ */
 public class TestGoalRemoved {
+        private final Club clubHome = new ClubImpl("home", null);
+    private final Club clubAway = new ClubImpl("away", null);
+    private final Match match = new MatchImpl(clubHome, clubAway);
+    private final Scoreboard scoreboard = match.getScore();
+    private final Boxes goalRemovedBox = new GoalRemovedBox(30);
+
     
+    @BeforeEach
+    public void setUpCurrentPlayer() {
+
+        //Setting initially the current club the home club, and fixing match status
+        //to have the homeClub as the current Club
+        if(match.getCurrentPlayer() != clubHome) {
+            match.turn();
+        }
+    }
+
+    private void verifyResult(final int homeResult,
+                            final int guestResult,
+                            final Club actualCurrentClub) {
+        assertEquals(homeResult, scoreboard.getHomeScore());
+        assertEquals(guestResult, scoreboard.getGuestScore());
+        assertEquals(actualCurrentClub, match.getCurrentPlayer());
+    }
+
+    @Test
+    public void testUnderZero() {
+
+        verifyResult(0, 0, match.getClubHome());
+        
+        //Scores must not go below zero
+        goalRemovedBox.event(match);
+        verifyResult(0, 0, match.getClubAway());
+
+        goalRemovedBox.event(match);
+        verifyResult(0, 0, match.getClubHome());
+
+    }
+
+    @Test
+    public void testBoxEvent() {
+
+        scoreboard.setHomeScore(2);
+        scoreboard.setGuestScore(1);
+
+        //Testing GoalRemovedBox for each team, assuming they use it
+        //consecutevely on it in the match
+        verifyResult(2, 1, match.getClubHome());
+        
+        goalRemovedBox.event(match);
+        verifyResult(1, 1, match.getClubAway());
+
+        goalRemovedBox.event(match);
+        verifyResult(1, 0, match.getClubHome());
+
+    }
+
+    @Test
+    public void testBoxPosition() {
+        assertEquals(30, goalRemovedBox.getPosition());
+    }
+
+    @Test
+    public  void testBoxName() {
+        assertEquals("Goal Removed", goalRemovedBox.getName());
+    }        
+        
+    @Test
+    public void testBoxImage() {
+        assertEquals("casella_33.png", goalRemovedBox.getImage());
+    }
 }
