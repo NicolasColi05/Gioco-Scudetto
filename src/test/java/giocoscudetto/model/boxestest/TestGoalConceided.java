3f270ea77df63ca5a5b2db80aeb6186dbe04f695
Gianluca Poli <gianluca.poli8@studio.unibo.it>
2026-05-26 12:40:57 +0200

    TestGoalConceided completed

--- src/test/java/giocoscudetto/model/boxestest/TestGoalConceided.java
+++ src/test/java/giocoscudetto/model/boxestest/TestGoalConceided.java
@@ -1,5 +1,81 @@
 package giocoscudetto.model.boxestest;
 
+import org.junit.jupiter.api.Test;
+
+import giocoscudetto.model.api.Boxes;
+import giocoscudetto.model.api.Club;
+import giocoscudetto.model.api.Match;
+import giocoscudetto.model.api.Scoreboard;
+import giocoscudetto.model.impl.ClubImpl;
+import giocoscudetto.model.impl.MatchImpl;
+import giocoscudetto.model.impl.boxes.GoalConceidedBox;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+
+/*
+ * CHECKSTYLE: MagicNumber OFF
+ * The above comment shuts down checkstyle: in a test suite, magic numbers may be tolerated.
+ */
+/**
+ * Test for {@link giocoscudetto.model.impl.boxes.GoalConceidedBox}.
+ */
 public class TestGoalConceided {
+ 
+    private final Club clubHome = new ClubImpl("home", null);
+    private final Club clubAway = new ClubImpl("away", null);
+    private final Match match = new MatchImpl(clubHome, clubAway);
+    private final Scoreboard scoreboard = match.getScore();
+    private final Boxes goalConceidedBox = new GoalConceidedBox(24);
+
     
+    @BeforeEach
+    public void setUpCurrentPlayer() {
+
+        //Setting initially the current club the home club, and fixing match status
+        //to have the homeClub as the current Club
+        if(match.getCurrentPlayer() != clubHome) {
+            match.turn();
+        }
+    }
+
+    @Test
+    public void testBoxEvent() {
+
+        int homeClubScore = scoreboard.getHomeScore();
+        int awayClubScore = scoreboard.getGuestScore();
+
+        //Testing GoalConceidedBox for each team, assuming they use it
+        //consecutevely on it in the match
+        assertEquals(match.getClubHome(), match.getCurrentPlayer());
+
+        goalConceidedBox.event(match);
+
+        assertEquals(homeClubScore, scoreboard.getHomeScore());
+        assertEquals(awayClubScore + 1, scoreboard.getGuestScore());
+        assertEquals(match.getClubAway(), match.getCurrentPlayer());
+
+        goalConceidedBox.event(match);
+
+        assertEquals(homeClubScore + 1, scoreboard.getHomeScore());
+        assertEquals(awayClubScore + 1, scoreboard.getGuestScore());
+        assertEquals(match.getClubHome(), match.getCurrentPlayer());
+    }
+
+    @Test
+    public void testBoxPosition() {
+        assertEquals(24, goalConceidedBox.getPosition());
+    }
+
+    @Test
+    public  void testBoxName() {
+        assertEquals("Goal Conceded", goalConceidedBox.getName());
+    }        
+        
+    @Test
+    public void testBoxImage() {
+        assertEquals("casella_9.png", goalConceidedBox.getImage());
+    }
 }
+
