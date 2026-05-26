17ae8b65070b01c9ee51e3e9ef49e47862422467
Gianluca Poli <gianluca.poli8@studio.unibo.it>
2026-05-26 13:22:44 +0200

    TestEmptyBox completed

---
+++ src/test/java/giocoscudetto/model/boxestest/TestEmptyBox.java
@@ -0,0 +1,77 @@
+package giocoscudetto.model.boxestest;
+
+import org.junit.jupiter.api.Test;
+
+import giocoscudetto.model.api.Boxes;
+import giocoscudetto.model.api.Club;
+import giocoscudetto.model.api.Match;
+import giocoscudetto.model.api.Scoreboard;
+import giocoscudetto.model.impl.ClubImpl;
+import giocoscudetto.model.impl.MatchImpl;
+import giocoscudetto.model.impl.boxes.CesariniBox;
+import giocoscudetto.model.impl.boxes.EmptyBox;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+
+/*
+ * CHECKSTYLE: MagicNumber OFF
+ * The above comment shuts down checkstyle: in a test suite, magic numbers may be tolerated.
+ */
+/**
+ * Test for {@link giocoscudetto.model.impl.boxes.EmptyBox}.
+ */
+public class TestEmptyBox {
+    
+    private final Club clubHome = new ClubImpl("home", null);
+    private final Club clubAway = new ClubImpl("away", null);
+    private final Match match = new MatchImpl(clubHome, clubAway);
+    private final Scoreboard scoreboard = match.getScore();
+    private final Boxes emptyBox = new EmptyBox(0);
+
+    
+    @BeforeEach
+    public void setUpCurrentPlayer() {
+
+        //Setting homeClub as the match current club
+        if(match.getCurrentPlayer() != clubHome) {
+            match.turn();
+        }
+    }
+
+    private void verifyResult(final int homeResult,
+                            final int guestResult,
+                            final Club actualCurrentClub) {
+        assertEquals(homeResult, scoreboard.getHomeScore());
+        assertEquals(guestResult, scoreboard.getGuestScore());
+        assertEquals(actualCurrentClub, match.getCurrentPlayer());
+    }
+
+    @Test
+    public void testBoxEvent() {
+
+        scoreboard.setHomeScore(2);
+        scoreboard.setGuestScore(2);
+
+        //Testing Initial Values
+        verifyResult(2, 2, match.getClubHome());
+
+        //Testing EmptyBox for each team, assuming they use it
+        //consecutevely on it in the match
+        emptyBox.event(match);
+        verifyResult(2, 2, match.getClubAway());
+
+        emptyBox.event(match);
+        verifyResult(2, 2, match.getClubHome());
+
+    }
+
+    @Test
+    public  void testBoxName() {
+        assertEquals("empty box", emptyBox.getName());
+    }        
+
+}
+
+
