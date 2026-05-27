70c3b04095b1ac44bb8d3a8a7b529a4b261bd991
Gianluca Poli <gianluca.poli8@studio.unibo.it>
2026-05-27 13:33:48 +0200

    Fixed the TableTest Implementation, adding a private method setUp and two more test

--- src/test/java/giocoscudetto/model/TestTable.java
+++ src/test/java/giocoscudetto/model/TestTable.java
@@ -22,46 +22,73 @@ import giocoscudetto.model.impl.TableImpl;
  */
 public class TestTable {
     
-private Club inter;
-   
-    private Club juve;
-    private Club bologna;
-    private Table rank;
+    private final Club inter = new ClubImpl("Inter", new PawnImpl(1));
+    private final Club juve = new ClubImpl("Juve", new PawnImpl(1));
+    private final Club bologna = new ClubImpl("Bologna", new PawnImpl(1));
+    private final Table rank = new TableImpl();;
 
-    @BeforeEach
-    void setUp() {
-        inter = new ClubImpl("Inter", new PawnImpl(1)); 
-        juve = new ClubImpl("Juve", new PawnImpl(1)); 
-        bologna = new ClubImpl("Bologna", new PawnImpl(1)); 
-        rank = new TableImpl();
+    private void setUp(final int juveP, final int interP, final int bolognaP,
+                       final int juveS, final int interS, final int bolognaS,
+                       final int juveC, final int interC, final int bolognaC ) {
+        
         rank.addAllClubs(List.of(bologna, inter, juve));
+
+        //Setting inter's point and net diff
+        inter.incrementPoints(interP);
+        inter.changeNetDiffs(interS, interC);
+
+        //Setting juve's point and net diff
+        juve.incrementPoints(juveP);
+        juve.changeNetDiffs(juveS, juveC);
+
+        //Setting bologna's point and net diff
+        bologna.incrementPoints(bolognaP);
+        bologna.changeNetDiffs(bolognaS, bolognaC);
     }
 
     @Test
-    void testCorrectClubsPosition() {
-        initialConfiguration();
+    public void testInitialClubsPosition() {
+
+        setUp(5, 6, 3,
+              2, 3, 2,
+              1, 2, 0);
 
         //After getting the initial configuration, we update Clubs rank position
         rank.updateClubRank();
-        assertEquals(rank.showPosition(), List.of(inter, juve, bologna));
+
+        assertEquals(List.of(inter, juve, bologna), rank.showPosition());
 
         //If points are the same net diff matter
         bologna.incrementPoints(3);
         rank.updateClubRank();
-        assertEquals(rank.showPosition(), List.of(bologna, inter, juve));
+
+        assertEquals( List.of(bologna, inter, juve), rank.showPosition());
     }
 
-    private void initialConfiguration() {
-        //Inter with 4 point and 1 net diff
-        inter.incrementPoints(4);
-        inter.changeNetDiffs(4, 3);
+    @Test
+    public void testNegativeNetDiff() {
 
-        //Juve with 3 points and 0 net diff
-        juve.incrementPoints(3);
-        juve.changeNetDiffs(2, 2);
+        setUp(3, 3, 1,
+              0, 1, 2,
+              1, 3, 0);
 
-        //Bologna with 1 point and 3 net diff
-        bologna.incrementPoints(1);
-        bologna.changeNetDiffs(5, 2);
+        rank.updateClubRank();
+
+        assertEquals(List.of(juve, inter, bologna), rank.showPosition());
+
+    }
+
+    @Test
+    public void testEqualPointsAndNetDiff() {
+
+        setUp(2, 2, 3,
+              1, 1, 2,
+              0, 0, 0);
+
+        //If point and net diff are the same we consider the name of the club
+        rank.updateClubRank();
+
+        assertEquals(List.of(bologna, juve, inter), rank.showPosition());
+
     }
 }
