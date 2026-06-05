98c978be209be49244e257615ff63951ea05413f
Federico Cecchini <federico.cecchini7@studio.unibo.it>
2026-06-05 14:29:07 +0200

    fixed checkstyle

--- src/test/java/giocoscudetto/model/boxestest/TestSuspendMatchBox.java
+++ src/test/java/giocoscudetto/model/boxestest/TestSuspendMatchBox.java
@@ -14,51 +14,70 @@ import giocoscudetto.model.impl.MatchImpl;
 import giocoscudetto.model.impl.PawnImpl;
 import giocoscudetto.model.impl.boxes.SuspendMatchBox;
 
-public class TestSuspendMatchBox {
+/*
+ * CHECKSTYLE: MagicNumber OFF
+ * The above comment shuts down checkstyle: in a test suite, magic numbers may be tolerated.
+ */
+/**
+ * Simple test for {@link SuspendMatchBox}.
+ */
+class TestSuspendMatchBox {
 
     private static final int BOX_POSITION = 10;
 
     private Club clubHome;
     private Club clubAway;
-    private Pawn pawnHome;
-    private Pawn pawnAway;
     private Match match;
     private Boxes suspendMatchBox;
 
     @BeforeEach
-    public void setUp() {
-        pawnHome = new PawnImpl(0);
-        pawnAway = new PawnImpl(0);
+    void setUp() {
+        final Pawn pawnHome = new PawnImpl(0);
+        final Pawn pawnAway = new PawnImpl(0);
         pawnAway.setPosition(22);
         pawnHome.setPosition(16);
+
         clubHome = new ClubImpl("home", pawnHome);
         clubAway = new ClubImpl("away", pawnAway);
+
         match = new MatchImpl(clubHome, clubAway);
         suspendMatchBox = new SuspendMatchBox(BOX_POSITION);
         match.setGoalHome(3);
         match.setGoalAway(2);
-        if(match.getCurrentPlayer() != clubHome) {
+        if (!match.getCurrentPlayer().equals(clubHome)) {
             match.turn();
         }
     }
 
+    /**
+     * Tests the get name.
+     */
     @Test
-    public void testGetName(){
+    void testGetName() {
         assertEquals("Suspend Match", suspendMatchBox.getName());
     }
 
+    /**
+     * Tests the get position.
+     */
     @Test
-    public void testGetPosition(){
+    void testGetPosition() {
         assertEquals(BOX_POSITION, suspendMatchBox.getPosition());
     }
 
+    /**
+     * Tests the get image.
+     */
     @Test
-    public void testGetImage(){
+    void testGetImage() {
         assertEquals("casella_10.png", suspendMatchBox.getImage());
     }
 
+    /**
+     * Tests the box event.
+     */
     @Test
-    public void testEventGameMode() {
+    void testEventGameMode() {
         suspendMatchBox.event(match);
         assertEquals(0, clubHome.getPawn().getPosition());
         assertEquals(0, clubAway.getPawn().getPosition());
