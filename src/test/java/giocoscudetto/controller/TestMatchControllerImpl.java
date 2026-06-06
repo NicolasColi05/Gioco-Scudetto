30e983508967877490925986a4646ed781e9741e
Federico Cecchini <federico.cecchini7@studio.unibo.it>
2026-06-06 11:14:45 +0200

    working on test match controller

--- src/test/java/giocoscudetto/controller/TestMatchControllerImpl.java
+++ src/test/java/giocoscudetto/controller/TestMatchControllerImpl.java
@@ -1,12 +1,20 @@
 package giocoscudetto.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import giocoscudetto.controller.api.CreateUpdateController;
 import giocoscudetto.controller.api.MatchController;
 import giocoscudetto.controller.impl.CreateUpdateControllerImpl;
 import giocoscudetto.controller.impl.MatchControllerImpl;
+import giocoscudetto.model.api.Match;
 
 /**
  * Test for {@link giocoscudetto.controller.impl.MatchControllerImpl}.
@@ -19,9 +27,13 @@ public class TestMatchControllerImpl {
     private static final String JUVENTUS = "juventus";
 
     private CreateUpdateController updateController = new CreateUpdateControllerImpl();
+    private MatchController matchController;
+    private List<String> listOfClubs;
+    private String currentClub;
 
+    @BeforeEach
     void setUp(){
-        final List<String> listOfClubs = new ArrayList<>();
+        listOfClubs = new ArrayList<>();
         listOfClubs.add(ROMA);
         listOfClubs.add(INTER);
         listOfClubs.add(NAPOLI);
@@ -33,7 +45,27 @@ public class TestMatchControllerImpl {
         listOfColors.add(3);
         listOfColors.add(4);
 
-        updateController.createClubs(listOfClubs, listOfColors);
-        MatchController matchController = new MatchControllerImpl(updateController);
+        this.updateController.createClubs(listOfClubs, listOfColors);
+        this.matchController = new MatchControllerImpl(updateController);
+    }
+
+    @Test
+    void test(){
+        matchController.setMatch();
+        assertEquals(updateController.getFixture().getCurrentMatch().getClubHome().getName(), this.matchController.getHomeName());
+        assertEquals(updateController.getFixture().getCurrentMatch().getClubAway().getName(), this.matchController.getGuestName());
+        this.matchController.setHelpFlag(true);
+        assertTrue(this.matchController.isHelpFlag());
+        assertEquals(Match.GameMode.NONE.name(), this.matchController.getGameMode());
+        for (String club : listOfClubs) {
+            if(club.equals(this.matchController.getCurrentPlayer())) {
+                currentClub = club;
+            }
+        }
+        int dice = this.matchController.move();
+        assertEquals(this.updateController.getFixture().getCurrentMatch().getCurrentPlayer().getPawn().getPosition(), dice);
+
+        this.matchController.checkBox();
+        
     }
 }
